2a5dcdb423d13b05f35efaee82be8c06896b231a
Kaisa Kangas <kkangas@protonmail.com>
2026-01-04 18:56:57 +0200

    Combined data added

--- Fysiikan_loppuprojekti.py
+++ Fysiikan_loppuprojekti.py
@@ -19,18 +19,19 @@ def butter_highpass_filter(data, cutoff,  nyq, order):
     y = filtfilt(b, a, data)
     return y
 
-df_a = pd.read_csv('Kiihtyvyys.csv')
-df_m = pd.read_csv('GPSData.csv')
+#df_a = pd.read_csv('Kiihtyvyys.csv')
+#df_m = pd.read_csv('GPSData.csv')
+df = pd.read_csv('combined.csv')
 
 st.title('Pikku happihyppely pakkasessa')
 
 #Keskinopeus ja kuljettu matka
-st.write("Keskinopeus on :", df_m['Speed (m/s)'].mean(),'m/s' )
-st.write("Kokonaismatka on :", df_m['Distance (km)'].max(),'km' )
+st.write("Keskinopeus on :", df['Speed (m/s)'].mean(),'m/s' )
+st.write("Kokonaismatka on :", df['Distance (km)'].max(),'km' )
 
-data = df_a['Linear Acceleration y (m/s^2)']
-T_tot = df_a['Time (s)'].max() #Koko datan pituus
-n = len(df_a['Time (s)']) #Datapisteiden lukumäärä
+data = df['Linear Acceleration y (m/s^2)']
+T_tot = df['Time (s)'].max() #Koko datan pituus
+n = len(df['Time (s)']) #Datapisteiden lukumäärä
 fs = n/T_tot #Näytteenottotaajus
 nyq = fs/2 #Nyqvistin taajuus
 order = 3
@@ -45,8 +46,8 @@ for i in range(n-1):
 
 st.write('Askelten määrä laskettuna suodatuksen avulla:', jaksot , 'askelta')
 
-signal = df_a['Linear Acceleration y (m/s^2)']
-t = df_a['Time (s)']
+signal = df['Linear Acceleration y (m/s^2)']
+t = df['Time (s)']
 N = len(signal)
 dt = np.max(t)/N
 
@@ -62,7 +63,7 @@ steps =  f_max*np.max(t) #Askelmäärä
 
 st.write('Askelten määrä laskettuna Fourier-analyysin avulla: ', np.round(steps), 'askelta')
 
-distance = df_m['Distance (km)'].max()
+distance = df['Distance (km)'].max()
 askelpituus = (distance/steps)*100000
 
 st.write('Askelpituus on ', np.round(askelpituus), 'cm')
@@ -76,11 +77,15 @@ st.line_chart(data_filt, y_label = 'Suodatettu data y, m/s^2',x_label = 'Aika [a
 #st.line_chart(df_m, x = 'Time (s)', y = 'Distance (km)', y_label = 'Distance',x_label = 'Time')
 
 #Luodaan kartta
-start_lat = df_m['Latitude (°)'].mean()
-start_long = df_m['Longitude (°)'].mean()
+map_df = df.dropna(
+    subset=["Latitude (°)", "Longitude (°)"]
+)
+
+start_lat = map_df['Latitude (°)'].mean()
+start_long = map_df['Longitude (°)'].mean()
 map = folium.Map(location = [start_lat,start_long], zoom_start = 14)
 
-folium.PolyLine(df_m[['Latitude (°)','Longitude (°)']], color = 'blue', weight = 3.5, opacity = 1).add_to(map)
+folium.PolyLine(map_df[['Latitude (°)','Longitude (°)']], color = 'blue', weight = 3.5, opacity = 1).add_to(map)
 
 st.title('Karttakuva happihyppelystä')
 
